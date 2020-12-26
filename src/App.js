802c8d3f2fa2f23301d8440a82f473c266687d53
ripterdust <bryantello2010@hotmail.com>
2020-12-25 18:50:53 -0600

    Agreagdo react-router-dom y mi carpeta de estilos con sass

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import {BrowserRoter as Roter, Switch, Route, Router} from 'react-router-dom';
+// My styles
+import './styles/main.css';
+
+function App() {
+  return (
+    <Router>
+
+
+      <Switch>
+        <Route></Route>
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
